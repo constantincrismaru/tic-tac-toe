b392cf04ed8fc07983bfd94c9caf0fa0d45efaca
Constantin Crismaru <constantin.crismaru@pentalog.com>
2021-06-28 16:14:01 +0300

    added home component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Play from './components/play'
 import Results from './components/results'
+import Home from './components/home'
 import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/tic-tac-toe">
         <div>
           <div>
             <NavLink className="app-link" exact={true} activeClassName='active-link' to='/play'>Play</NavLink>
             <NavLink className="app-link" activeClassName='active-link' to='/results'>Results</NavLink>
           </div>
           <Switch>
+            <Route exact path="/" component={Home} />
             <Route path="/play" component={Play} />
             <Route path="/results" component={Results} />
           </Switch>
---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return <div>
+        <h2>Home</h2>
+        <div>
+            Play or see results.
+        </div>
+    </div>
+}
+
+export default Home
